964a1a16aaa79d5621f00850150297693a9298a2
mecebeci <mecv2@yahoo.com>
2025-11-08 13:29:07 +0300

    Serve index and static assets; add health check endpoint

--- main.go
+++ main.go
@@ -8,14 +8,29 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./index.html")
+	})
+
+	mux.Handle("/app/assets/", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
+
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
 	}
 
-	log.Println("Server listening on http://localhost:8080")
+	log.Println("listening on http://localhost:8080")
+	log.Println("  - health:  GET /healthz")
+	log.Println("  - app:     GET /app")
+	log.Println("  - assets:  GET /app/assets/")
 
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed{
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
 }
